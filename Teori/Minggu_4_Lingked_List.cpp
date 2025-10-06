0256a19df9b7d1842f33519c32bf8e853d387d73
RizkiAgungPrasetya <rizky.agungprastya@gmail.com>
2025-10-06 10:02:06 +0700

    update

---
+++ Teori/Minggu_4_Lingked_List.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+struct charML{
+    string nama;
+    charML *next;
+} *head, *tail, *curr;
+
+int main(){
+    curr = new charML;
+    curr->nama = "Lucan";
+    curr->next = NULL;
+    head = tail = curr;
+
+    curr = new charML;
+    curr->nama = "Miya";
+    curr->next = NULL;
+    tail->next = curr;
+    tail = curr;
+
+    curr = new charML;
+    curr->nama = "Zilong";
+    curr->next = NULL;
+    tail->next = curr;
+    tail = curr;
+
+    curr = new charML;
+    curr->nama = "Valir";
+    curr->next = NULL;
+    tail->next = curr;
+    tail = curr;
+
+    cout << "Nama Hero ML:" << endl;
+    curr = head;
+    while(curr != NULL){
+        cout << curr->nama << endl;
+        curr = curr->next;
+    }
+
+    return 0;
+}
---
+++ Teori/tempCodeRunnerFile.cpp
@@ -0,0 +1,1 @@
+    cout << "Nama Hero ML:" << endl;
